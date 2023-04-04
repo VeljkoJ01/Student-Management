b244935905cf975fef495345c4a933c6bd9a67fe
VeljkoJ01 <129805803+VeljkoJ01@users.noreply.github.com>
2023-04-04 16:15:50 +0200

    Add files via upload

---
+++ Student Management/StudentManagement/src/main/java/com/example/StudentManagement/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.StudentManagement.service;
+
+import com.example.StudentManagement.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<Student> getAllStudents();
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
+
+}
